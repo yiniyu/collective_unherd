2f217922bac599b3b589ea3a201d801aebb732cd
Raghu Betina <raghu@firstdraft.com>
2019-06-09 22:38:57 +0000

    Only committee user can modify committee

--- app/controllers/committees_controller.rb
+++ app/controllers/committees_controller.rb
@@ -1,4 +1,14 @@
 class CommitteesController < ApplicationController
+  before_action :current_user_must_be_committee_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_committee_user
+    committee = Committee.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == committee.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @committees = Committee.all
 
