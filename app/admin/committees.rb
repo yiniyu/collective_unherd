0d1d943ecdec49362cb92681c1539f22b78488ab
Raghu Betina <raghu@firstdraft.com>
2019-06-09 22:38:47 +0000

    Whitelist columns in Committee for ActiveAdmin

--- app/admin/committees.rb
+++ app/admin/committees.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Committee do
+
+ permit_params :name, :board_members_id, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
